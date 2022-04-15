41d11173308f1e4a67c41dfb070b800feccf0370
Messy Soup <messysoup@protonmail.com>
2022-04-14 23:14:33 -0400

    add all-metadata api call

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 	router.GET("/book/title/:title", middleware.Get_by_book_title)
 	router.GET("/book/id/:id", middleware.Get_book_by_id)
 	router.GET("/book/number_of_books", middleware.Get_number_of_books)
+	router.GET("/book/all_book_metadata", middleware.Get_All_Metadata)
 
 	router.Run("localhost:8080")
 }
