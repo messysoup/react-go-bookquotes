ce72f909c31c76960ab30a80f6f42acfaf023ed7
Messy Soup <messysoup@protonmail.com>
2022-04-14 22:52:42 -0400

    insert styling from text and sanitize output

--- Frontend/src/Components/blockquote.js
+++ Frontend/src/Components/blockquote.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import DOMPurify from 'dompurify';
 
 import { Container } from "react-bootstrap";
 import axios from 'axios';
@@ -8,15 +9,45 @@ import { useState, useEffect } from "react";
 
 const BlockQuote = (props) => {
 
-    const [ quote, set_quote ] = useState({})
+    const [ quote, setQuote ] = useState({})
+
+    // FIXME: Rendered with dangerouslysethtml currently.  XSS shouldn't be an issue
+    // as it is sanitized using DOMPurify directly before rendering.
+    const italicized = (q) => {
+        let result = "";
+        let currentlyItalicized = false;
+
+        console.log("quote", quote.Quote)
+
+        for (const e of q) {
+            if (e === "_" && currentlyItalicized === false) {
+                result = result + "<i>"
+                currentlyItalicized = true
+            } else if (e === "_" && currentlyItalicized) {
+                result = result + "</i>"
+                currentlyItalicized = false
+            } else {
+                result = result + e
+            }
+        }
+
+        return result
+    }
+
+    const getBookData = async () => {
+        const r = Math.floor(Math.random() * (props.numofbooks)).toString()
+
+        let resp = await axios.get("/book/id/" + r)
+
+        resp.data.Quote = italicized(resp.data.Quote)
+
+        setQuote(resp.data)
+    }
 
     useEffect(() => {
-        console.log("props", props.numofbooks)
-        const r = Math.floor(Math.random() * (props.numofbooks)).toString()
-        
-        axios.get("/book/id/" + r).then( res => {
-            set_quote(res.data)
-        })
+
+        getBookData()
+
     }, [])
 
     return (
@@ -25,7 +56,7 @@ const BlockQuote = (props) => {
         <div className="App">
             <Container>
                 <blockquote className="blockquote text-center">
-                    <p>{quote.Quote}</p>
+                    <p dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(quote.Quote)}} />
                     <footer className="blockquote-footer">{quote.Author}: <cite>{quote.Title}</cite> </footer>
                 </blockquote>
             </Container>
