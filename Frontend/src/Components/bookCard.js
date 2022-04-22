2eac8e3ba6b28ca8db13f515d19b569e79a48953
Messy Soup <messysoup@protonmail.com>
2022-04-22 00:33:53 -0400

    display max lines of a quote in a card

--- Frontend/src/Components/bookCard.js
+++ Frontend/src/Components/bookCard.js
@@ -4,8 +4,6 @@ import italicized from "../Helpers/Italicize";
 
 const BookCard = (props) => {
 
-    console.log(props.books)
-
     const filename = props.books.BookFile.replace(".txt", "")
 
     return (
@@ -18,7 +16,7 @@ const BookCard = (props) => {
                 <div className="card-body">
                     <h5 className="card-title">{props.books.Title}</h5>
                     <h6 className="card-subtitle">{props.books.Author}</h6>
-                    <p style={{padding: "10px 0px 0px 0px"}} dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(italicized(props.books.Quote))}} />
+                    <p className="book-card-quote" style={{padding: "10px 0px 0px 0px"}} dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(italicized(props.books.Quote))}} />
                     <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                 </div>
             </Col>
